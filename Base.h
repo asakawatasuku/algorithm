041b682754ea8ed5701c8e5b8e10c7bc5b50d716
asakawatasuku <akuma.fairy.tail@gmail.com>
2017-09-26 15:40:06 +0900

    Baseクラスの追加

--- Base.h
+++ Base.h
@@ -1,17 +1,13 @@
 #pragma once
-#include <d3d11_1.h>
 #include <SimpleMath.h>
 
 class Base
 {
 private:
-	static const float SPEED;
-private:
-	float m_speed;	// 速度
-	DirectX::SimpleMath::Vector3 m_velocity;
+	float m_speed;			
+	DirectX::SimpleMath::Vector3 m_velocity;	// 速度
 
 protected:
-	void Initiarize();
-	virtual void Move();		// 移動関数
+	virtual void Update() = 0;	// 更新関数
 };
 
